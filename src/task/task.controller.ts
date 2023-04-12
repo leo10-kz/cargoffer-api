9d8a0accd352d31212e110ce1212bbddc60ec03a
leonel <leitochaso10@gmail.com>
2023-04-12 18:37:34 +0200

    Routes Put and Delete done

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   Res,
   HttpStatus,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/task.dto';
 import { TaskService } from './task.service';
@@ -15,7 +16,7 @@ import { TaskService } from './task.service';
 @Controller('task')
 export class TaskController {
   constructor(private taskServise: TaskService) {}
-  @Post('/create')
+  @Post('/')
   async createTask(@Res() res, @Body() task: CreateTaskDto) {
     const response = await this.taskServise.createTask(task);
 
@@ -32,8 +33,34 @@ export class TaskController {
   }
 
   @Get('/:id')
-  async getTask(@Res() res, @Param('id') id) {
+  async getTask(@Res() res, @Param('id') id: string) {
     const response = await this.taskServise.getTask(id);
+
+    if (!response) throw new NotFoundException('Task not found');
+
     return res.status(HttpStatus.OK).json({ Task: response });
+  }
+
+  @Put('/:id')
+  async updateTask(
+    @Res() res,
+    @Body() task: CreateTaskDto,
+    @Param('id') id: string,
+  ) {
+    const response = await this.taskServise.updateTask(id, task);
+    if (!response) throw new NotFoundException('Task does not exist');
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: 'Update successfully', Task: response });
+  }
+
+  @Delete('/:id')
+  async deleteTask(@Res() res, @Param('id') id: string) {
+    const response = await this.taskServise.deleteTask(id);
+    if (!response) throw new NotFoundException('Task does not exist');
+
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: 'Delete successfully', Task: response });
   }
 }
